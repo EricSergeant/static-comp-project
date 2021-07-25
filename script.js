907b516ef926fc12beda57dc3b8d62efa6f57c4d
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-07-24 20:03:50 -0500

    Create script.js and populate data structure in file

---
+++ script.js
@@ -0,0 +1,19 @@
+let modules = [
+  { image: './assets/Tomb-of-horrors copy.jpg',
+    title: 'Tomb of Horrors',
+    likes: 02,
+    date: 'Saturday 07/24/21 8:00pm',
+    link: 'https://www.drivethrurpg.com/product/170945/S14-Dungeons-of-Dread-1e',
+    levels: '10 - 14',
+    code: 'S1' 
+  },
+  { image: './assets/the-lost-city copy.jpg',
+  title: 'The Lost City',
+  likes: 18,
+  date: 'Saturday 07/24/21 8:00pm',
+  link: 'https://www.drivethrurpg.com/product/17084/B4-The-Lost-City-Basic',
+  levels: '1 - 3',
+  code: 'B4' 
+}
+];
+
